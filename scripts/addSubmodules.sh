72bf8eaf3ee45ac7caf6905b885f684e4258544e
SDRausty <sdrausty@gmail.com>
2019-01-06 20:52:11 -0500

    new file:   scripts/addSubmodules.sh

---
+++ scripts/addSubmodules.sh
@@ -0,0 +1,21 @@
+#!/data/data/com.termux/files/usr/bin/sh  
+# Copyright 2017 (c) all rights reserved 
+# by S D Rausty https://sdrausty.github.io
+# Update submodules to latest version. 
+#####################################################################
+git submodule add https://github.com/sdrausty/buildAPKsApps sources/apps
+git submodule add https://github.com/sdrausty/buildAPKsAmusements sources/amusements
+git submodule add https://github.com/sdrausty/buildAPKsBrowsers sources/browsers 
+git submodule add https://github.com/sdrausty/buildAPKsClocks sources/clocks
+git submodule add https://github.com/sdrausty/buildAPKsCompasses sources/compasses 
+git submodule add https://github.com/sdrausty/buildAPKsFlashlights sources/flashlights 
+git submodule add https://github.com/sdrausty/buildAPKsGames sources/games 
+git submodule add https://github.com/sdrausty/buildAPKsLiveWallpapers sources/liveWallpapers
+git submodule add https://github.com/sdrausty/buildAPKsSamples sources/samples 
+git submodule add https://github.com/sdrausty/buildAPKsTop10 sources/top10 
+git submodule add https://github.com/sdrausty/buildAPKsDevTools sources/tools 
+git submodule add https://github.com/sdrausty/buildAPKsTutorials sources/tutorials
+git submodule add https://github.com/sdrausty/buildAPKsWidgets sources/widgets
+git submodule add https://github.com/sdrausty/buildAPKsArtifacts libs
+git submodule add https://github.com/sdrausty/docsBuildAPKs docs
+git submodule add https://github.com/sdrausty/maintenanceBuildAPKs scripts/maintenance
