f807892425f7df30e01855b770eae3c462e63572
SDRausty <sdrausty@gmail.com>
2019-01-09 18:25:25 -0500

    new file:   scripts/build/buildAllInDir.sh

--- scripts/build/buildAllInDir.sh
+++ scripts/build/buildAllInDir.sh
@@ -1,9 +1,40 @@
-#!/bin/env bash 
+#!/data/data/com.termux/files/usr/bin/env bash 
 # Copyright 2017-2019 (c) all rights reserved 
 # by S D Rausty https://sdrausty.github.io
 #####################################################################
-set -Eeu
+set -Eeuo pipefail
 
+_STRPERROR_() { # Run on script error.
+	printf "\\e[?25h\\e[1;7;38;5;0mbuildAPKs buildAllInDir.sh ERROR:  Signal $? received!\\e[0m\\n"
+	exit 201
+}
+
+_STRPEXIT_() { # Run on exit.
+	_CLUPA_
+	printf "\\e[?25h\\e[0m"
+	set +Eeuo pipefail 
+	exit
+}
+
+_STRPSIGNAL_() { # Run on signal.
+	printf "\\e[?25h\\e[1;7;38;5;0mbuildAPKs buildAllInDir.sh WARNING:  Signal $? received!\\e[0m\\n"
+ 	exit 211 
+}
+
+_STRPQUIT_() { # Run on quit.
+	printf "\\e[?25h\\e[1;7;38;5;0mbuildAPKs buildAllInDir.sh WARNING:  Quit signal $? received!\\e[0m\\n"
+ 	exit 221 
+}
+
+trap "_STRPERROR_ $LINENO $BASH_COMMAND $?" ERR 
+trap _STRPEXIT_ EXIT
+trap _STRPSIGNAL_ HUP INT TERM 
+trap _STRPQUIT_ QUIT 
+
+_CLUPA_() { # Run on quit.
+ 	rmdir "$TMPDIR"/buildAPKsLibs 2>/dev/null &
+	_WAKEUNLOCK_
+}
 _WAKELOCK_() {
 	_PRINTWLA_ 
 	am startservice --user 0 -a com.termux.service_wake_lock com.termux/com.termux.app.TermuxService 1>/dev/null
@@ -20,6 +51,11 @@ _PRINTDONE_() {
 	printf "\033[1;32mDONE  \033[0m\n\n"
 }
 
+_PRINTP_() {
+	printf "\n\033[1;34mPopulating %s/buildAPKsLibs: " "$TMPDIR"
+	printf '\033]2;Populating %s/buildAPKsLibs: OK\007' "$TMPDIR"
+}
+
 _PRINTWLA_() {
 	printf "\n\033[1;34mActivating termux-wake-lock: "'\033]2;Activating termux-wake-lock: OK\007'
 }
@@ -29,6 +65,18 @@ _PRINTWLD_() {
 }
 
 _WAKELOCK_
+if [ ! -e "$TMPDIR"/buildAPKsLibs ]
+then
+	mkdir "$TMPDIR"/buildAPKsLibs 
+	_PRINTP_
+	cd "$TMPDIR"/buildAPKsLibs 
+	find /storage/ -name "*.aar" -exec ln -s {} \; ||:
+	find /storage/ -name "*.jar" -exec ln -s {} \; ||:
+	 _PRINTDONE_
+fi
+# 	find /storage/ -name "*.aar" -exec ln -s {} \; 2>> stnd.dir.build.err."$(date +%s)".log ||:
+# 	find /storage/ -name "*.jar" -exec ln -s {} \; 2>> stnd.dir.build.err."$(date +%s)".log ||:
+_PRINTDONE_
 # cd "$HOME"/buildAPKs
 # echo Updating buildAPKs.
 # git submodule update --init -- ./sources/apps
@@ -45,6 +93,5 @@ _WAKELOCK_
 # git submodule update --init -- ./sources/tutorials
 # git submodule update --init -- ./sources/widgets
 # cd "$HOME"/buildAPKs/sources
-find .  -name AndroidManifest.xml \
-	-execdir "$PWD"/buildOne.sh {} \;
-_WAKEUNLOCK_
+/bin/env /bin/find . -name AndroidManifest.xml -execdir /bin/bash "$PWD"/buildOne.sh "$@" {} \;
+# 2>stnderr"$(date +%s)".log
--- scripts/build/buildOne.sh
+++ scripts/build/buildOne.sh
@@ -5,11 +5,20 @@
 set -Eeuo pipefail
 
 _STRPERROR_() { # Run on script error.
+	printf "\\e[?25h\\e[1;7;38;5;0mbuildAPKs buildOne.sh ERROR:  Signal $? received!\\e[0m\\n"
 	printf "\\e[?25h\\e[0m\\n"
 	exit 201
 }
 
 _STRPEXIT_() { # Run on exit.
+	sleep 1
+	printf "\e[1;38;5;151m%s\n\e[0m" "Cleaning up."
+ 	rm -rf bin 2>/dev/null ||: & 
+	rm -rf gen 2>/dev/null ||: & 
+ 	rm -rf obj 2>/dev/null ||: & 
+#  	rmdir assets 2>/dev/null ||:
+# 	rmdir res 2>/dev/null ||:
+	printf "\e[1;38;5;151mCompleted tasks in %s\n\n\e[0m" "$PWD"
 	printf "\\e[?25h\\e[0m"
 	set +Eeuo pipefail 
 	exit
@@ -30,10 +39,10 @@ trap _STRPEXIT_ EXIT
 trap _STRPSIGNAL_ HUP INT TERM 
 trap _STRPQUIT_ QUIT 
 printf "\n\e[1;38;5;116mBeginning build in %s\n" "$PWD"
-if [ ! -e "./assets" ]
-then
-	find . -type d -name assets -exec ln -s {} \;
-fi
+# if [ ! -e "./assets" ]
+# then
+# 	find . -type d -name assets -exec ln -s {} \;
+# fi
 if [ ! -e "./assets" ]
 then
 	mkdir ./assets
@@ -46,34 +55,41 @@ if [ ! -d "./gen" ]
 then
 	mkdir ./gen
 fi
+if [ ! -d "./obj" ]
+then
+	mkdir ./obj
+fi
 if [ ! -d "./res" ]
 then
 	mkdir ./res
 fi
-if [ ! -d "./src" ] && [ -d "./java" ]
+# if [ ! -d "./src" ] && [ -d "./java" ]
+# then
+# 	ln -s java src
+# fi
+if [ ! -d "/sdcard/Download/builtAPKs"$1"" ]
 then
-	ln -s java src
-fi
-if [ ! -d "/sdcard/Download/builtAPKs" ]
-then
-	mkdir /sdcard/Download/builtAPKs
+	mkdir /sdcard/Download/builtAPKs"$1"
 fi
 printf "\e[1;38;5;115m%s\n\e[0m" "aapt: begun"
 aapt package -f \
-	--min-sdk-version 3 \
 	-M ./AndroidManifest.xml \
-	-I "$PREFIX"/share/java/android.jar \
 	-J gen \
 	-S res \
 	-m
 printf "\e[1;38;5;148m%s\n\e[1;38;5;114m%s\n\e[0m" "aapt: done" "ecj: begun"
-ecj -d ./obj -classpath "$PREFIX"/share/java/android.jar \
-	-sourcepath . "$(find . -type f -name "*.java")"
+if [ -d "$TMPDIR"/buildAPKsLibs ]
+then
+	ecj -d ./obj -sourcepath . "$(find . -type f -name "*.java")" -classpath "$(find "$TMPDIR"/buildAPKsLibs -name "*.jar")"
+else
+	ecj -d ./obj -sourcepath . "$(find . -type f -name "*.java")"
+fi
 printf "\e[1;38;5;149m%s\n\e[1;38;5;113m%s\n\e[0m" "ecj: done" "dx: begun"
-dx --dex --min-sdk-version=4 --output=./bin/classes.dex ./obj
+dx --dex --output=./bin/classes.dex ./obj
 printf "\e[1;38;5;148m%s\n\e[1;38;5;112m%s\n\e[0m" "dx: done" "Making the apk."
 aapt package -f \
-	--min-sdk-version 3 \
+	--min-sdk-version 13 \
+	--target-sdk-version 23 \
 	-M ./AndroidManifest.xml \
 	-S ./res \
 	-A ./assets \
@@ -83,15 +99,7 @@ cd bin
 aapt add -f step2.apk classes.dex
 printf "\e[1;38;5;114m%s\n" "Signing step2.apk"
 apksigner ../step2-debug.key step2.apk ../step2.apk
-sleep 1
 cd ..
-cp step2.apk /sdcard/Download/builtAPKs/step"$now".apk
-printf "\e[1;38;5;115mCopied to /sdcard/Download/builtAPKs/step%s.apk\n" "$now"
-printf "\e[1;38;5;149mYou can install it from /sdcard/Download/builtAPKs/step%s.apk\n" "$now" 
-sleep 1
-printf "\e[1;38;5;151m%s\n\e[0m" "Cleaning up."
-rm -rf bin 
-rm -rf gen 
-rm -rf obj 
-printf "\e[1;38;5;151mCompleted build in %s\n\n\e[0m" "$PWD"
-
+cp step2.apk /sdcard/Download/builtAPKs"$1"/step"$now".apk
+printf "\e[1;38;5;115mCopied to /sdcard/Download/builtAPKs"$1"/step%s.apk\n" "$now"
+printf "\e[1;38;5;149mYou can install it from /sdcard/Download/builtAPKs"$1"/step%s.apk\n" "$now" 
