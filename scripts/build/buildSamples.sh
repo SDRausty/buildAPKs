035c6301e688d4849e72f5954b39ae6cd5d477e0
SDRausty <sdrausty@gmail.com>
2019-01-12 12:58:21 -0500

    modified:   buildSamples.sh

--- scripts/build/buildSamples.sh
+++ scripts/build/buildSamples.sh
@@ -3,11 +3,12 @@
 # by S D Rausty https://sdrausty.github.io
 #####################################################################
 set -e 
-cd $HOME/buildAPKs
+cd "$HOME"/buildAPKs
 echo Updating buildAPKs.
 git pull
 git submodule update --init -- ./sources/samples
 git submodule update --init -- ./scripts/maintenance
 git submodule update --init -- ./docs
-find $HOME/buildAPKs/sources/samples/  -name AndroidManifest.xml \
-	-execdir $HOME/buildAPKs/buildOne.sh Tutorials {} \; 2>stnderr"$(date +%s)".log
+find "$HOME"/buildAPKs/sources/samples/  -name AndroidManifest.xml \
+	-execdir "$HOME"/buildAPKs/buildOne.sh Samples {} \; 2>stnderr"$(date +%s)".log
+"$HOME"/buildAPKs/scripts/maintenance/fa.sh Samples
