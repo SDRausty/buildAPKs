c56968f3d19b55c16091986d77be7ff0b448da81
SDRausty <sdrausty@gmail.com>
2019-01-11 22:04:36 -0500

    modified:   buildAllInDir.sh

--- buildAllInDir.sh
+++ buildAllInDir.sh
@@ -1,4 +1,4 @@
-#!/data/data/com.termux/files/usr/bin/env bash 
+#!/bin/env bash 
 # Copyright 2017-2019 (c) all rights reserved 
 # by S D Rausty https://sdrausty.github.io
 #####################################################################
