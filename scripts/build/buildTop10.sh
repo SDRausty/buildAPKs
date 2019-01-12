d3e6ef3c5ad46435962c896d37c4db98f9bdd502
SDRausty <SDRausty@gmail.com>
2019-01-12 04:20:53 -0500

    modified:   buildApps.sh 	modified:   buildBrowsers.sh 	modified:   buildClocks.sh

--- scripts/build/buildTop10.sh
+++ scripts/build/buildTop10.sh
@@ -1,4 +1,4 @@
-#!/bin/env sh
+#!/bin/env bash
 # Copyright 2017-2019 (c) all rights reserved 
 # by S D Rausty https://sdrausty.github.io
 #####################################################################
