aa5fe3a8e9837e0cb9989d031abbdf19898b33bf
SDRausty <sdrausty@gmail.com>
2019-01-14 16:09:31 -0500

    new file:   .hushupdates modified:   buildOne.sh

--- buildOne.sh
+++ buildOne.sh
@@ -49,12 +49,12 @@ _STRPEXIT_() { # Run on exit.
 }
 
 _STRPSIGNAL_() { # Run on signal.
-	printf "\\e[?25h\\e[1;7;38;5;0mbuildAPKs %s WARNING:  Signal %s received!\\e[0m\\n" "$?" "${0##*/}"
+	printf "\\e[?25h\\e[1;7;38;5;0mbuildAPKs %s WARNING:  Signal %s received!\\e[0m\\n" "${0##*/}" "$?"
  	exit 221 
 }
 
 _STRPQUIT_() { # Run on quit.
-	printf "\\e[?25h\\e[1;7;38;5;0mbuildAPKs %s WARNING:  Quit signal %s received!\\e[0m\\n" "$?" "${0##*/}"
+	printf "\\e[?25h\\e[1;7;38;5;0mbuildAPKs %s WARNING:  Quit signal %s received!\\e[0m\\n" "${0##*/}" "$?"
  	exit 222 
 }
 
