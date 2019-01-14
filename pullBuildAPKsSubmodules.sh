deb006209a529567b44a325baf7c8d2e6a462360
SDRausty <sdrausty@gmail.com>
2019-01-14 01:36:57 -0500

    modified:   pullBuildAPKsSubmodules.sh

--- pullBuildAPKsSubmodules.sh
+++ pullBuildAPKsSubmodules.sh
@@ -7,11 +7,14 @@ set -e
 cd "$HOME"/buildAPKs
 echo Updating buildAPKs\; "\`${0##*/}\` might need to load sources from submodule repositories into buildAPKs. This may take a little while to complete. Please be patient if this script needs to download source code from https://github.com"
 git pull --recurse-submodules
+git submodule update --init -- ./docs
+git submodule update --init -- ./libs
+git submodule update --init -- ./scripts/maintenance
 git submodule update --init -- ./sources/applications
-git submodule update --init -- ./sources/entertainment
 git submodule update --init -- ./sources/browsers 
 git submodule update --init -- ./sources/clocks
 git submodule update --init -- ./sources/compasses 
+git submodule update --init -- ./sources/entertainment
 git submodule update --init -- ./sources/flashlights 
 git submodule update --init -- ./sources/games 
 git submodule update --init -- ./sources/liveWallpapers
@@ -20,5 +23,3 @@ git submodule update --init -- ./sources/top10
 git submodule update --init -- ./sources/tools 
 git submodule update --init -- ./sources/tutorials
 git submodule update --init -- ./sources/widgets
-git submodule update --init -- ./scripts/maintenance
-git submodule update --init -- ./docs
