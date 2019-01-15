d78ba4966c937dcb95b6b74869e693d07e0e831f
SDRausty <SDRausty@gmail.com>
2019-01-14 19:52:16 -0500

    modified:   buildMyFirstAPKs.sh

--- buildMyFirstAPKs.sh
+++ buildMyFirstAPKs.sh
@@ -2,12 +2,10 @@
 # Copyright 2017-2019 (c) all rights reserved 
 # by S D Rausty https://sdrausty.github.io
 #####################################################################
-set -Eeuo pipefail
-shopt -s nullglob globstar
+set -e
 JID=MyFirstAPKs
 mkdir -p  "$HOME"/buildAPKs/var/log
 find "$HOME"/buildAPKs/sources/Apps/ -name AndroidManifest.xml \
 	-execdir "$HOME"/buildAPKs/buildOne.sh "$JID" {} \; \
 	2> "$HOME"/buildAPKs/var/log/stnderr.buildMyFirstAPKs."$(date +%s)".log
-
 #EOF
