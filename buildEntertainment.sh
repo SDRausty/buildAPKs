b513f674001b9f791f4967917d0d98887df4807c
SDRausty <sdrausty@gmail.com>
2019-01-16 02:42:51 -0500

    modified:   buildEntertainment.sh

--- buildEntertainment.sh
+++ buildEntertainment.sh
@@ -5,77 +5,54 @@
 set -Eeuo pipefail
 shopt -s nullglob globstar
 
-_STRPERROR_() { # Run on script error.
-	printf "\\e[?25h\\e[1;7;38;5;0mbuildAPKs buildAllInDir.sh ERROR:  Signal $? received!\\e[0m\\n"
+_SETRPERROR_() { # Run on script error.
+	local RV="$?"
+	printf "\\e[?25h\\e[1;7;38;5;0mbuildAPKs buildAllInDir.sh ERROR:  Signal %s received!\\e[0m\\n" "${0##*/}" "$RV"
 	exit 201
 }
 
-_STRPEXIT_() { # Run on exit.
+_SETRPEXIT_() { # Run on exit.
 	_WAKEUNLOCK_
 	printf "\\e[?25h\\e[0m"
 	set +Eeuo pipefail 
 	exit
 }
 
-_STRPSIGNAL_() { # Run on signal.
-	printf "\\e[?25h\\e[1;7;38;5;0mbuildAPKs buildAllInDir.sh WARNING:  Signal $? received!\\e[0m\\n"
+_SETRPSIGNAL_() { # Run on signal.
+	local RV="$?"
+	printf "\\e[?25h\\e[1;7;38;5;0mbuildAPKs %s WARNING:  Signal %s received!\\e[0m\\n" "${0##*/}" "$RV"
  	exit 211 
 }
 
-_STRPQUIT_() { # Run on quit.
-	printf "\\e[?25h\\e[1;7;38;5;0mbuildAPKs buildAllInDir.sh WARNING:  Quit signal $? received!\\e[0m\\n"
+_SETRPQUIT_() { # Run on quit.
+	local RV="$?"
+	printf "\\e[?25h\\e[1;7;38;5;0mbuildAPKs %s WARNING:  Quit signal %s received!\\e[0m\\n" "${0##*/}" "$RV"
  	exit 221 
 }
 
-trap '_STRPERROR_ $LINENO $BASH_COMMAND $?' ERR 
-trap _STRPEXIT_ EXIT
-trap _STRPSIGNAL_ HUP INT TERM 
-trap _STRPQUIT_ QUIT 
+trap '_SETRPERROR_ $LINENO $BASH_COMMAND $?' ERR 
+trap _SETRPEXIT_ EXIT
+trap _SETRPSIGNAL_ HUP INT TERM 
+trap _SETRPQUIT_ QUIT 
 
-_WAKELOCK_() {
-	_PRINTWLA_ 
-	am startservice --user 0 -a com.termux.service_wake_lock com.termux/com.termux.app.TermuxService 1>/dev/null
-	touch "$HOME/buildAPKs/var/lock/wakelock.$PPID"
-	_PRINTDONE_ 
-}
-
-_WAKEUNLOCK_() {
-	_PRINTWLD_ 
-	am startservice --user 0 -a com.termux.service_wake_unlock com.termux/com.termux.app.TermuxService 1>/dev/null
-	rm -f "$HOME/buildAPKs/var/lock/wakelock.$PPID"
-	_PRINTDONE_ 
-}
-
-_PRINTDONE_() {
-	printf "\\033[1;32mDONE  \\033[0m\\n"
-}
-
-_PRINTWLA_() {
-	printf "\\n\\033[1;34mActivating termux-wake-lock: "'\033]2;Activating termux-wake-lock: OK\007'
-}
-
-_PRINTWLD_() {
-	printf "\\n\\033[1;34mReleasing termux-wake-lock: "'\033]2;Releasing termux-wake-lock: OK\007'
-}
-
-JID=Entertainment
+git submodule update --init -- ./scripts/shlibs
+. "$RDR/scripts/shlibs/lock.sh"
+JID=Entertainment # job id
+SHNAME="${0##*/}" # shell script name
+WDR="$RDR/sources/entertainment/"
 _WAKELOCK_
-cd "$HOME/buildAPKs"
-if [[ ! -f "$HOME/buildAPKs/sources/entertainment/.git" ]]
+cd "$RDR" # Change directory to root directory.
+if [[ ! -f "$RDR/sources/entertainment/.git" ]]
 then
-	echo
-	echo "Updating buildAPKs\; \`${0##*/}\` might need to load sources from submodule repositories into buildAPKs. This may take a little while to complete. Please be patient if this script needs to download source code from https://github.com"
-	git pull 
+	_PRINTUMODS_
+	_UMODS_
 	git submodule update --init -- ./sources/entertainment
-	git submodule update --init -- ./scripts/maintenance
-	git submodule update --init -- ./scripts/shlibs
-	git submodule update --init -- ./docs
 else
-	echo
-	echo "To update module ~/buildAPKs/sources/entertainment to the newest version remove the ~/buildAPKs/sources/entertainment/.git file and run ${0##*/} again."
+	_PRINTNMODS_
 fi
-find "$HOME"/buildAPKs/sources/entertainment/ -name AndroidManifest.xml \
-	-execdir "$HOME/buildAPKs/buildOne.sh" "$JID" {} \; \
-	2> "$HOME/buildAPKs/var/log/stnderr.buildEntertainment.$(date +%s).log"
+find "$WDR" -name AndroidManifest.xml \
+	-execdir "$RDR/buildOne.sh" "$JID" {} \; \
+	2> "$RDR/var/log/stnderr.buildEntertainment.$(date +%s).log"
+. "$RDR/scripts/shlibs/fa.sh" "$JID" "$WDR"
 
 #EOF
