f1b562359aecda2700fdb781b0444eb4ec47ebd0
sdrausty <sdrausty@gmail.com>
2019-01-18 00:34:59 -0500

    renamed:    sources/a.bash -> a.bash modified:   pullBuildAPKsSubmodules.sh

--- pullBuildAPKsSubmodules.sh
+++ pullBuildAPKsSubmodules.sh
@@ -6,7 +6,7 @@
 set -e 
 mkdir -p  "$HOME"/buildAPKs/var/log
 cd "$HOME"/buildAPKs
-if [[ ! -f "$HOME/buildAPKs/docs/.git" ]] || [[ ! -f "$HOME/buildAPKs/scripts/maintenance/.git" ]] || [[ ! -f "$HOME/buildAPKs/sources/applications/.git" ]] || [[ ! -f "$HOME/buildAPKs/sources/browsers/.git" ]] || [[ ! -f "$HOME/buildAPKs/sources/clocks/.git" ]] || [[ ! -f "$HOME/buildAPKs/sources/compasses/.git" ]] || [[ ! -f "$HOME/buildAPKs/sources/entertainment/.git" ]] || [[ ! -f "$HOME/buildAPKs/sources/flashlights/.git" ]] || [[ ! -f "$HOME/buildAPKs/sources/games/.git" ]] || [[ ! -f "$HOME/buildAPKs/sources/liveWallpapers/.git" ]] || [[ ! -f "$HOME/buildAPKs/sources/samples/.git" ]] || [[ ! -f "$HOME/buildAPKs/sources/top10/.git" ]] || [[ ! -f "$HOME/buildAPKs/sources/tools/.git" ]] || [[ ! -f "$HOME/buildAPKs/sources/tutorials/.git" ]] || [[ ! -f "$HOME/buildAPKs/sources/widgets/.git" ]]
+if [[ ! -f "$HOME/buildAPKs/docs/.git" ]] || [[ ! -f "$HOME/buildAPKs/scripts/shlibs/.git" ]] || [[ ! -f "$HOME/buildAPKs/scripts/maintenance/.git" ]] || [[ ! -f "$HOME/buildAPKs/sources/applications/.git" ]] || [[ ! -f "$HOME/buildAPKs/sources/browsers/.git" ]] || [[ ! -f "$HOME/buildAPKs/sources/clocks/.git" ]] || [[ ! -f "$HOME/buildAPKs/sources/compasses/.git" ]] || [[ ! -f "$HOME/buildAPKs/sources/entertainment/.git" ]] || [[ ! -f "$HOME/buildAPKs/sources/flashlights/.git" ]] || [[ ! -f "$HOME/buildAPKs/sources/games/.git" ]] || [[ ! -f "$HOME/buildAPKs/sources/liveWallpapers/.git" ]] || [[ ! -f "$HOME/buildAPKs/sources/samples/.git" ]] || [[ ! -f "$HOME/buildAPKs/sources/top10/.git" ]] || [[ ! -f "$HOME/buildAPKs/sources/tools/.git" ]] || [[ ! -f "$HOME/buildAPKs/sources/tutorials/.git" ]] || [[ ! -f "$HOME/buildAPKs/sources/widgets/.git" ]]
 then
 	printf "\\n\\nUpdating buildAPKs\; \`%s\` might need to load sources from module repositories into buildAPKs. This may take a little while to complete. Please be patient if this script needs to download source code from https://github.com\\n" "${0##*/}"
 	git pull 
@@ -14,7 +14,7 @@ then
 	git submodule update --init -- ./scripts/maintenance
 	git submodule update --init -- ./scripts/shlibs
 	git submodule update --init -- ./sources/applications
-	git submodule update --init -- ./sources/browsers 
+	git submodule update --init --recursive ./sources/browsers 
 	git submodule update --init -- ./sources/clocks
 	git submodule update --init -- ./sources/compasses 
 	git submodule update --init -- ./sources/entertainment
@@ -27,8 +27,9 @@ then
 	git submodule update --init -- ./sources/tutorials
 	git submodule update --init -- ./sources/widgets
 else
-	printf "\\n\\nTo update the modules in ~/buildAPKs to the newest version remove one of these .git files:\\n\\n"
+	printf "\\n\\nTo update the modules in ~/buildAPKs to the newest version remove these .git files:\\n\\n"
 	find "$HOME/buildAPKs" -type f -name .git
+	printf "\\n\\n	find . -type f -name .git -exec rm {} \\\;\\n\\n"
 
 fi
 
