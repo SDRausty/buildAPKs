fa3bc8d274349851cdbd187436acc31c1f40ca0d
SDRausty <sdrausty@gmail.com>
2019-01-20 20:47:40 -0500

    modified:   buildClocks.sh modified:   buildCompasses.sh modified:   du.sha modified:   du.sha256 modified:   du.sha512 modified:   du.tar.gz

--- buildCompasses.sh
+++ buildCompasses.sh
@@ -77,14 +77,15 @@ else
 	echo "To update module ~/buildAPKs/sources/clocks to the newest version remove the ~/buildAPKs/sources/compasses/.git file and run ${0##*/} again."
 fi
 
+WDR="$HOME/buildAPKs/sources/"
 _WAKELOCK_
 find "$HOME"/buildAPKs/sources/compasses -name AndroidManifest.xml \
 	-execdir "$HOME/buildAPKs/buildOne.sh" "$JID" {} \; \
-	2> "$HOME/buildAPKs/var/log/stnderr.build.${JID,,}.$NUM.log"
+	2> "$HOME/buildAPKs/var/log/stnderr.build$JID.$NUM.log"
 cd /data/data/com.termux/files/home/buildAPKs/sources/samples/android-code/Compass/
-"$HOME"/buildAPKs/buildOne.sh "$JID" 2> "$HOME/buildAPKs/var/log/stnderr.build.${JID,,}.$NUM.log"
+"$HOME"/buildAPKs/buildOne.sh "$JID" 2> "$HOME/buildAPKs/var/log/stnderr.build$JID.$NUM.log"
 cd /data/data/com.termux/files/home/buildAPKs/sources/samples/Compass/
-"$HOME"/buildAPKs/buildOne.sh "$JID" 2> "$HOME/buildAPKs/var/log/stnderr.build.${JID,,}.$NUM.log"
-. "$HOME/buildAPKs/scripts/shlibs/fa.sh" "$JID" "$HOME/buildAPKs/sources/" ||:
+"$HOME"/buildAPKs/buildOne.sh "$JID" 2> "$HOME/buildAPKs/var/log/stnderr.build$JID.$NUM.log"
+. "$HOME/buildAPKs/scripts/shlibs/fa.sh" "$JID" "$WDR" ||:
 
 #EOF
