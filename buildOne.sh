cc5962660c44ac2aad4250d00210f634d3c8bd55
SDRausty <sdrausty@gmail.com>
2019-01-20 23:05:59 -0500

    deleted:    a.bash modified:   buildOne.sh modified:   du.sha modified:   du.sha256 modified:   du.sha512 modified:   du.tar.gz

--- buildOne.sh
+++ buildOne.sh
@@ -148,6 +148,6 @@ cd ..
 cp step2.apk /storage/emulated/0/Download/builtAPKs/"$EXT$DAY"/step"$NOW".apk
 printf "\\e[1;38;5;115mCopied to /storage/emulated/0/Download/builtAPKs/"$EXT$DAY"/step%s.apk\\n" "$NOW"
 printf "\\e[1;38;5;149mYou can install it from /storage/emulated/0/Download/builtAPKs/"$EXT$DAY"/step%s.apk\\n" "$NOW" 
-printf "\\e[?25h\\e[1;7;38;5;34mShare %s here; Share everwhere%s!\\e[0m\\n" "https://wiki.termux.com/wiki/Development" "🌐"
+printf "\\e[?25h\\e[1;7;38;5;34mShare %s here; Share everwhere%s!\\e[0m\\n" "https://wiki.termux.com/wiki/Development" "🌍🌎🌏🌐"
 
 #EOF
