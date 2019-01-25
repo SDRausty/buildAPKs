37db8fea91b32c87064f2fa202748faa6f0424b7
S D Rausty <SDRausty@gmail.com>
2019-01-25 10:12:58 -0500

    modified:   pullBuildAPKsSubmodules.sh modified:   tree.md5 modified:   tree.sha modified:   tree.sha256 modified:   tree.sha512 modified:   tree.tar.gz

--- pullBuildAPKsSubmodules.sh
+++ pullBuildAPKsSubmodules.sh
@@ -8,7 +8,7 @@ mkdir -p  "$HOME"/buildAPKs/var/log
 cd "$HOME"/buildAPKs
 if [[ ! -f "$HOME/buildAPKs/docs/.git" ]] || [[ ! -f "$HOME/buildAPKs/scripts/shlibs/.git" ]] || [[ ! -f "$HOME/buildAPKs/scripts/maintenance/.git" ]] || [[ ! -f "$HOME/buildAPKs/sources/applications/.git" ]] || [[ ! -f "$HOME/buildAPKs/sources/browsers/.git" ]] || [[ ! -f "$HOME/buildAPKs/sources/clocks/.git" ]] || [[ ! -f "$HOME/buildAPKs/sources/compasses/.git" ]] || [[ ! -f "$HOME/buildAPKs/sources/entertainment/.git" ]] || [[ ! -f "$HOME/buildAPKs/sources/flashlights/.git" ]] || [[ ! -f "$HOME/buildAPKs/sources/games/.git" ]] || [[ ! -f "$HOME/buildAPKs/sources/livewallpapers/.git" ]] || [[ ! -f "$HOME/buildAPKs/sources/samples/.git" ]] || [[ ! -f "$HOME/buildAPKs/sources/top10/.git" ]] || [[ ! -f "$HOME/buildAPKs/sources/tools/.git" ]] || [[ ! -f "$HOME/buildAPKs/sources/tutorials/.git" ]] || [[ ! -f "$HOME/buildAPKs/sources/widgets/.git" ]]
 then
-	printf "\\n\\nUpdating buildAPKs\; \`%s\` might want to load sources from module repositories into buildAPKs. This may take a little while to complete. Please be patient if this script wants to download source code from https://github.com\\n" "${0##*/}"
+	printf "\\n\\nUpdating buildAPKs; \`%s\` might want to load sources from module repositories into buildAPKs. This may take a little while to complete. Please be patient if this script wants to download source code from https://github.com\\n" "${0##*/}"
 	git pull 
 	git submodule update --init -- ./docs
 	git submodule update --init -- ./scripts/maintenance
@@ -29,7 +29,7 @@ then
 else
 	printf "\\n\\nTo update the modules in ~/buildAPKs to the newest version remove these .git files:\\n\\n"
 	find "$HOME/buildAPKs" -type f -name .git
-	printf "\\n\\n	find . -type f -name .git -exec rm {} \\\;\\n\\n"
+	printf "\\n\\n	find . -type f -name .git -exec rm {} \\;\\n\\n"
 
 fi
 
