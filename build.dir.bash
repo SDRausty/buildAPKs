aa33b91f2df950190eac45007daa4b8695b13780
SD Rausty <SDRausty@gmail.com>
2019-02-04 17:05:40 -0500

    modified:   build.dir.bash modified:   buildOne.bash modified:   tree.md5 modified:   tree.sha256 modified:   tree.sha512 modified:   tree.tar.gz

--- build.dir.bash
+++ build.dir.bash
@@ -53,7 +53,7 @@ fi
 JDR="$PWD"
 find "$@" -name AndroidManifest.xml \
 	-execdir /bin/bash "$HOME/buildAPKs/buildOne.bash" "$JID" "$JDR" {} \; \
-	2> "$HOME/buildAPKs/var/log/stnderr."${JID,,}".$(date +%s).log"
+	2> "$HOME/buildAPKs/var/log/stnderr."${JID,,}".$NUM.log"
 . "$RDR/scripts/shlibs/faa.bash" "$JID" "$JDR"
 #	search: lowercase bash variable pattern replacement substitution site:tldp.org
 #	http://www.tldp.org/LDP/abs/html/bashver4.html#CASEMODPARAMSUB
--- buildOne.bash
+++ buildOne.bash
@@ -25,7 +25,9 @@ _SBOTRPEXIT_() { # Run on exit.
 	local RV="$?"
 	if [[ "$RV" != 0 ]]  
 	then 
-		printf "\\e[?25h\\e[1;7;38;5;0mbuildAPKs signal %s received by %s in %s.  More information in \`%s/var/log/stnderr.%s.%s.log\` file.\\e[0m\\n" "$RV" "${0##*/}" "$PWD" "$RDR" "${JID,,}" "$NUM"
+		printf "\\e[?25h\\e[1;7;38;5;0mbuildAPKs signal %s received by %s in %s.  More information in \`%s/var/log/stnderr.%s.%s.log\` file.\\n\\n" "$RV" "${0##*/}" "$PWD" "$RDR" "${JID,,}" "$NUM"
+		tail -n 16 "$RDR/var/log/stnderr.${JID,,}.$NUM.log"
+		printf "\\e[0m\\n\\n" 
 	fi
 	if [[ "$RV" = 220 ]]  
 	then 
