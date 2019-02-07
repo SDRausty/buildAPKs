10e8864917cb29d5b655ae64fc32fa9333c65493
S D Rausty <SDRausty@gmail.com>
2019-02-06 20:51:25 -0500

    modified:   buildOne.bash modified:   tree.md5 modified:   tree.sha256 modified:   tree.sha512 modified:   tree.tar.gz

--- buildOne.bash
+++ buildOne.bash
@@ -13,16 +13,18 @@ _SBOTRPERROR_() { # Run on script error.
 		mkdir -p "$RDR/var/tmp"
 		CER="conf.$NUM.err"
 		echo "$1 $2 $3 $RV" > "$RDR/var/tmp/$CER" # https://stackoverflow.com/questions/11162406/open-and-write-data-to-text-file-using-bash-shell-scripting
-		echo Fixing error...
+		echo Fixing ecj error...
 		echo Please wait a moment...
 		sleep 0.64
 		if [[ "$(command getprop ro.build.version.sdk)" -gt 22 ]] 
 		then
-			echo installing ecj
+			echo Installing package ecj4.6 which should behave much better on device than the current version.
  			. "$RDR/fix.ecj.error.bash"
+			echo "~/$SRDR/debs/ecj4.6_4.6.2_all.deb installed;  Continuing..."
 		else
-			echo installing ecj4.6
+			echo "Installing ~/$SRDR/debs/ecj4.6_4.6.2_all.deb which should behave much better on device than the current version.  Continuing..." 
  			. "$RDR/fix.ecj4.6.error.bash"
+			echo "~/$SRDR/debs/ecj4.6_4.6.2_all.deb installed;  Continuing..."
 		fi
 	else
 		printf "\\e[?25h\\e[1;7;38;5;0mbuildAPKs %s ERROR:  Signal %s received!  More information in \`%s/var/log/stnderr.%s.%s.log\` file.\\e[0m\\n" "${0##*/}" "$RV" "$RDR" "${JID,,}" "$NUM"
@@ -64,12 +66,12 @@ _SBOTRPEXIT_() { # Run on exit.
 		printf "\\e[?25h\\e[1;7;38;5;0mSignal 224 generated in %s;  Cannot run in $HOME!  See \`stnderr*.log\` file.\\n\\nRunning \`ls\`:\\n" "$PWD" "${0##*/}" "${0##*/}"
 	fi
 	sleep 1
-	printf "\\e[1;38;5;151m%s\\n\\e[0m" "Cleaning up."
+	printf "\\e[1;38;5;151m%s\\n\\e[0m" "Cleaning up..."
  	rm -rf ./bin 2>/dev/null ||:  
 	rm -rf ./gen 2>/dev/null ||:  
  	rm -rf ./obj 2>/dev/null ||:  
 	find . -name R.java -exec rm {} \; 2>/dev/null ||:  
-	printf "\\e[1;38;5;151mCompleted tasks in ~/%s\\n\\n\\e[0m" "${PWD:33}"
+	printf "\\e[1;38;5;151mCompleted tasks in ~/%s.\\n\\n\\e[0m" "${PWD:33}"
 	printf "\\e[?25h\\e[0m"
 	set +Eeuo pipefail 
 	exit 0
@@ -107,7 +109,7 @@ then
 	echo "Cannot run in $HOME!  Signal 224 generated in $PWD."
 	exit 224
 fi
-printf "\\n\\e[1;38;5;116mBeginning build in ~/%s\\n\\e[0m" "${PWD:33}"
+printf "\\n\\e[1;38;5;116mBeginning build in ~/%s...\\n\\e[0m" "${PWD:33}"
 if [[ ! -e "./assets" ]]
 then
 	mkdir -p ./assets
@@ -132,13 +134,13 @@ if [[ ! -d "/storage/emulated/0/Download/builtAPKs/$EXT$DAY" ]]
 then
 	mkdir -p "/storage/emulated/0/Download/builtAPKs/$EXT$DAY"
 fi
-printf "\\e[1;38;5;115m%s\\n\\e[0m" "aapt: started"
+printf "\\e[1;38;5;115m%s\\n\\e[0m" "aapt: started..."
 aapt package -f \
 	-M ./AndroidManifest.xml \
 	-J gen \
 	-S res \
 	-m
-printf "\\e[1;38;5;148m%s;  \\e[1;38;5;114m%s\\n\\e[0m" "aapt: done" "ecj: begun"
+printf "\\e[1;38;5;148m%s;  \\e[1;38;5;114m%s\\n\\e[0m" "aapt: done" "ecj: begun..."
 if [[ -d "$TMPDIR/buildAPKsLibs" ]] && [[ -d "$JDR/libs" ]] # directories exist
 then # loads artifacts
         ecj -d ./obj -classpath "$TMPDIR/buildAPKsLibs:$JDR/libs" -sourcepath . "$(find . -type f -name "*.java")"
@@ -148,9 +150,9 @@ then
 else
         ecj -d ./obj -sourcepath . "$(find . -type f -name "*.java")"
 fi
-printf "\\e[1;38;5;149m%s;  \\e[1;38;5;113m%s\\n\\e[0m" "ecj: done" "dx: started"
+printf "\\e[1;38;5;149m%s;  \\e[1;38;5;113m%s\\n\\e[0m" "ecj: done" "dx: started..."
 dx --dex --output=./bin/classes.dex ./obj
-printf "\\e[1;38;5;148m%s;  \\e[1;38;5;112m%s\\n\\e[0m" "dx: done" "Making the apk."
+printf "\\e[1;38;5;148m%s;  \\e[1;38;5;112m%s\\n\\e[0m" "dx: done" "Making the apk..."
 aapt package -f \
 	--min-sdk-version 1 \
 	--target-sdk-version 23 \
@@ -158,10 +160,10 @@ aapt package -f \
 	-S ./res \
 	-A ./assets \
 	-F bin/step2.apk
-printf "\\e[1;38;5;113m%s\\n\\e[0m" "Adding the classes.dex to the apk."
+printf "\\e[1;38;5;113m%s\\n\\e[0m" "Adding the classes.dex to the apk..."
 cd bin || exit
 aapt add -f step2.apk classes.dex
-printf "\\e[1;38;5;114m%s\\n" "Signing step2.apk"
+printf "\\e[1;38;5;114m%s\\n" "Signing step2.apk..."
 apksigner ../step2-debug.key step2.apk ../step2.apk
 cd ..
 cp step2.apk "/storage/emulated/0/Download/builtAPKs/$EXT$DAY/step$NOW.apk"
