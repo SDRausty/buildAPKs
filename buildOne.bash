6353c245019f4360a89f2c0271ebeddb821743bd
SD Rausty <SDRausty@gmail.com>
2019-05-31 00:16:29 -0400

    modified:   buildOne.bash

--- buildOne.bash
+++ buildOne.bash
@@ -182,6 +182,6 @@ cd ..
 cp step2.apk "/storage/emulated/0/Download/builtAPKs/$EXT$DAY/step$NOW.apk"
 printf "\\e[1;38;5;115mCopied to Download/builtAPKs/%s/step%s.apk\\n" "$EXT$DAY" "$NOW"
 printf "\\e[1;38;5;149mIt can be installed from Download/builtAPKs/%s/step%s.apk\\n" "$EXT$DAY" "$NOW" 
-printf "\\e[?25h\\e[1;7;38;5;34mShare %s here; Share everwhere%s!\\e[0m\\n" "https://wiki.termux.com/wiki/Development" "🌎🌍🌏🌐"
+printf "\\e[?25h\\e[1;7;38;5;34mShare %s everwhere%s!\\e[0m\\n" "https://wiki.termux.com/wiki/Development" "🌎🌍🌏🌐"
 
 #EOF
