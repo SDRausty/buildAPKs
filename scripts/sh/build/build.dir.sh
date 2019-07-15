16ded70fde1ccb41869e8ef903580388c19f7d60
SDRausty <27742457+sdrausty@users.noreply.github.com>
2019-07-15 08:43:58 -0400

    renamed:    scripts/sh/build/build.all.sh -> scripts/sh/build/build.dir.sh

--- scripts/sh/build/build.dir.sh
+++ scripts/sh/build/build.dir.sh
@@ -2,13 +2,12 @@
 # Copyright 2019 (c) all rights reserved 
 # by BuildAPKs https://buildapks.github.io/buildAPKs/
 # Contributeur HemanthJabalpuri @sdrausty/buildAPKs/issues/13
-# Invocation : $HOME/buildAPKs/scripts/sh/build/build.all.sh sources/
+# Invocation : $HOME/buildAPKs/scripts/sh/build/build.dir.sh sources/
 #####################################################################
 set -e
 
 for APP in $(find $1 -name "AndroidManifest.xml") 
 do 
-	echo $APP
 	$HOME/buildAPKs/scripts/sh/build/build.sh ${APP%/*} 
 done
 
