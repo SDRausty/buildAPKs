5511995da83a2ece704fea0a5ff8a456d2522268
SDRausty <27742457+SDRausty@users.noreply.github.com>
2019-09-24 00:30:26 -0400

    modified:   scripts/bash/init/pull.buildAPKs.modules.bash

--- scripts/bash/init/pull.buildAPKs.modules.bash
+++ scripts/bash/init/pull.buildAPKs.modules.bash
@@ -7,7 +7,7 @@ set -e
 
 declare -A SBMS # Declare associative array for available submoldules. 
 RDR="$HOME/buildAPKs/"
-SBMS=([scripts/bash/shlibs]="$HOME/buildAPKs/scripts/bash/shlibs/.git" [sources/applications]="$HOME/buildAPKs/sources/applications/.git" [sources/browsers]="$HOME/buildAPKs/sources/browsers/.git" [sources/clocks]="$HOME/buildAPKs/sources/clocks/.git" [sources/compasses]="$HOME/buildAPKs/sources/compasses/.git" [sources/entertainment]="$HOME/buildAPKs/sources/entertainment/.git" [sources/flashlights]="$HOME/buildAPKs/sources/flashlights/.git" [sources/games]="$HOME/buildAPKs/sources/games/.git" [sources/live.wallpapers]="$HOME/buildAPKs/sources/live.wallpapers/.git" [sources/samples]="$HOME/buildAPKs/sources/samples/.git" [sources/top10]="$HOME/buildAPKs/sources/top10/.git" [sources/tools]="$HOME/buildAPKs/sources/tools/.git" [sources/tutorials]="$HOME/buildAPKs/sources/tutorials/.git" [sources/widgets]="$HOME/buildAPKs/sources/widgets/.git")
+SBMS=([scripts/bash/shlibs]="$HOME/buildAPKs/scripts/bash/shlibs/.git" [sources/applications]="$HOME/buildAPKs/sources/applications/.git" [sources/browsers]="$HOME/buildAPKs/sources/browsers/.git" [sources/clocks]="$HOME/buildAPKs/sources/clocks/.git" [sources/compasses]="$HOME/buildAPKs/sources/compasses/.git" [sources/entertainment]="$HOME/buildAPKs/sources/entertainment/.git" [sources/flashlights4]="$HOME/buildAPKs/sources/flashlights4/.git" [sources/games]="$HOME/buildAPKs/sources/games/.git" [sources/live.wallpapers]="$HOME/buildAPKs/sources/live.wallpapers/.git" [sources/samples4]="$HOME/buildAPKs/sources/samples4/.git" [sources/samps]="$HOME/buildAPKs/sources/samps/.git" [sources/top10]="$HOME/buildAPKs/sources/top10/.git" [sources/tools]="$HOME/buildAPKs/sources/tools/.git" [sources/torches]="$HOME/buildAPKs/sources/torches/.git" [sources/tutorials]="$HOME/buildAPKs/sources/tutorials/.git" [sources/widgets]="$HOME/buildAPKs/sources/widgets/.git")
 
 _2GSU_() {
 	if [[ "$SBMI" = "" ]] 
@@ -38,25 +38,27 @@ _CK4MS_() { # ChecKs 4 ModuleS
 
 _GSMU_() {	
 	printf "\\n\\nUpdating buildAPKs; \`%s\` shall attempt to load sources from Github submodule repositories into ~/buildAPKs.  This may take a little while to complete.  Please be patient while \`%s\` downloads source code from https://github.com\\n" "${0##*/}" "${0##*/}"
-	(git pull 2>/dev/null && printf "\\nPlease wait: updating ~/buildAPKs...\\n") || (printf "\\nCannot update ~/buildAPKs: continuing...\\n")
+	(git pull 2>/dev/null && printf "\\nPlease wait; Updating ~/buildAPKs...\\n") || (printf "\\nCannot update ~/buildAPKs: Continuing...\\n")
 	_GSU_ ./scripts/bash/shlibs
 	_GSU_ ./sources/applications
 	_GSU_ ./sources/browsers 
 	_GSU_ ./sources/clocks
 	_GSU_ ./sources/compasses 
 	_GSU_ ./sources/entertainment
-	_GSU_ ./sources/flashlights 
+	_GSU_ ./sources/flashlights4
 	_GSU_ ./sources/games 
 	_GSU_ ./sources/live.wallpapers
-	_GSU_ ./sources/samples 
+	_GSU_ ./sources/samples4
+	_GSU_ ./sources/samps 
 	_GSU_ ./sources/top10 
 	_GSU_ ./sources/tools 
+	_GSU_ ./sources/torches
 	_GSU_ ./sources/tutorials
 	_GSU_ ./sources/widgets
 }
 
 _GSU_() { # update submodules to latest version
-	(git submodule update --init --recursive --remote $1 2>/dev/null) ||  (printf "\\n\\n\\e[1;7;38;5;66m%s%s\\e[0m\\n" "Cannot update ~/buildAPKs${1:1}: continuing...") # https://www.tecmint.com/chaining-operators-in-linux-with-practical-examples/
+	(git submodule update --init --recursive --remote $1 2>/dev/null) ||  (printf "\\n\\n\\e[1;7;38;5;66m%s%s\\e[0m\\n" "Cannot update ~/buildAPKs${1:1}: Continuing...") # https://www.tecmint.com/chaining-operators-in-linux-with-practical-examples/
 }
 
 cd "$RDR"
