5d01ade87ba956b31a9e8a134158c21cde2e58d2
SDRausty <27742457+sdrausty@users.noreply.github.com>
2019-10-15 20:11:44 -0400

    modified:   do.sums.bash 	modified:   scripts/bash/components/build.github.orgs.bash 	modified:   scripts/bash/components/build.github.users.bash 	modified:   ztree.sha512.sum

--- do.sums.bash
+++ do.sums.bash
@@ -6,9 +6,8 @@
 # To check the files use; sha512sum -c ztree.sha512.sum
 #####################################################################
 set -eu
-git pull
 rm -f *.sum
-FILELIST=( $(find . -type f | grep -v .git | sort) )
+FILELIST=( $(find . -type f | grep -vw .git | sort) )
 CHECKLIST=(sha512sum)
 for SCHECK in ${CHECKLIST[@]}
 do
@@ -17,15 +16,17 @@ do
 	do
 		$SCHECK "$FILE" >> ztree.${SCHECK::-3}.sum
 	done
+	chmod 444 ztree.${SCHECK::-3}.sum
 done
 for SCHECK in  ${CHECKLIST[@]}
 do
 	printf  "\\n%s\\n" "Checking $SCHECK..."
 	$SCHECK -c ztree.${SCHECK::-3}.sum
 done
+git pull
 git add .
 git commit
 git push
 ls
 printf "\\e[1;38;5;112m%s\\e[0m\\n" "$PWD"
-# do.sums.bash EOF
+# do.sums.sh EOF
