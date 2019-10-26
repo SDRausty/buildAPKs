5ad6c55f3e52beaa33c4232bd972f56fb1714f6c
SDRausty <27742457+sdrausty@users.noreply.github.com>
2019-10-25 22:04:45 -0400

    modified:   conf/VERSIONID 	deleted:    md5.sum 	renamed:    do.sum.bash -> scripts/maintenance/do.sums.bash 	new file:   scripts/maintenance/vgen.sh 	new file:   sha512.sum

--- scripts/maintenance/do.sums.bash
+++ scripts/maintenance/do.sums.bash
@@ -1,15 +1,16 @@
 #!/bin/env bash
 # Copyright 2019 (c) all rights reserved by S D Rausty; see LICENSE  
 # https://sdrausty.github.io hosted courtesy https://pages.github.com
-# To create the checksum file and commit use; ./do.sum.bash
-# To see the file tree use; awk '{ print $2 }' md5.sum
-# To check the files use; md5sum -c md5.sum
+# To create checksum files and commit use; ./do.sums.bash
+# To see file tree use; awk '{print $2}' sha512.sum
+# To check the files use; sha512sum -c sha512.sum
 #####################################################################
 set -eu
 git pull
+./scripts/maintenance/vgen.sh
 rm -f *.sum
-FILELIST=( $(find . -type f | grep -vw .git | sort) )
-CHECKLIST=(md5sum)
+FILELIST=( $(find . -type f | grep -v .git | sort) )
+CHECKLIST=(sha512sum)
 for SCHECK in ${CHECKLIST[@]}
 do
  	printf "%s\\n" "Creating $SCHECK file: Please wait a moment..."
@@ -17,8 +18,8 @@ do
 	do
 		$SCHECK "$FILE" >> ${SCHECK::-3}.sum
 	done
-	chmod 400 ${SCHECK::-3}.sum
 done
+chmod 400 ${SCHECK::-3}.sum 
 for SCHECK in  ${CHECKLIST[@]}
 do
 	printf  "\\n%s\\n" "Checking $SCHECK..."
@@ -29,4 +30,4 @@ git commit
 git push
 ls
 printf "\\e[1;38;5;112m%s\\e[0m\\n" "$PWD"
-# do.sum.sh EOF
+# do.sums.bash EOF
