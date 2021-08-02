d5a217ab358ee4d280e2a8b3cb384b961d9e2c30
S D Rausty <27742457+SDRausty@users.noreply.github.com>
2021-08-02 16:03:07 -0400

    Update build.one.bash

--- scripts/bash/build/build.one.bash
+++ scripts/bash/build/build.one.bash
@@ -4,6 +4,7 @@
 #####################################################################
 set -Eeuo pipefail
 shopt -s nullglob globstar
+unset JAVA_HOME
 [ -z "${RDR:-}" ] && RDR="$HOME/buildAPKs"
 [ "$PWD" = "$PREFIX" ] || [ "$PWD" = "$HOME" ] || [ "$PWD" = "$RDR" ] && printf "\\e[?25h\\e[1;7;38;5;0mSignal 224 generated in %s;  Cannot run in folder %s; %s exiting...\\e[0m\\n" "$PWD" "$PWD" "${0##*/} build.in.dir.bash" && exit 224
 
